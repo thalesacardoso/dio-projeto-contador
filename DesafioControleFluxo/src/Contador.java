d431622f095e9f10a631af5aa8250f812f555fa3
thalesacardoso <thales.dev@hotmail.com>
2023-11-22 16:07:58 -0300

    Finalização do projeto

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -16,6 +16,8 @@ public class Contador {
       } catch (ParametrosInvalidosException exception) {
          System.out.println("O segundo parâmetro deve ser maior que o primeiro");
       }
+
+      terminal.close();
    }
 
    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
